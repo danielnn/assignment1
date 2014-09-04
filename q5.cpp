7f0fbf75275e2e5e022f248f57379534b8fc1d9e
Daniel Nguyen <danielnn@usc.edu>
2014-09-04 04:22:41 -0700

    finished assigning bucket numbers

--- q5.cpp
+++ q5.cpp
@@ -5,11 +5,40 @@
 
 using namespace std;
 
+
+struct bucket{
+
+	int* contents = NULL;
+	int contentSize = 0;
+	int bucketNumber = 0;
+};
+
 int numBuckets = 0;
 int numCommands = 0;
 
+
+void assignBucketNumbers(bucket* item)
+{
+	for(int i = 0; i < numBuckets; i++)
+	{
+		item[i].bucketNumber = i + 1;
+		cout << item[i].bucketNumber << endl;
+	}
+
+}
+
+void emptyBucket(bucket* item, int bucketNumber)
+{
+
+	item->contentSize = 0;
+}
+
 int main(int argc, char* argv[])
 {
+
+	bucket* buckets = NULL;
+	string* commandList = NULL;
+
 	if( argc != 2)
 	{
 		cout<<"Incorrect command line input" << endl;
@@ -23,13 +52,24 @@ int main(int argc, char* argv[])
 		cout << "Improper file name inputted, please try again" << endl;
 		return 1;
 	}
-
+// get data for number of buckets and commands
 	iFile >> numBuckets;
 	iFile >> numCommands;
+// dynamically allocate number of buckets and commands
 
-	cout<< numBuckets << endl;
-	cout << numCommands << endl;
-	
+	buckets = new bucket [numBuckets];
+	commandList = new string [numCommands];
+
+	assignBucketNumbers(buckets);
+
+
+
 	iFile.close();
+
+	delete [] buckets;
+	delete [] commandList;
+
 	return 0;
 }
+
+
